b2f5b7a2ce8a5d62ea8b80479b8b233ea2aa8dae
MarvinL.com <contact@marvinl.com>
2024-02-08 22:17:18 -0400

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-
 import robotsTxt from "astro-robots-txt";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress(), robotsTxt()],
+  output: "server",
+  adapter: netlify()
 });
