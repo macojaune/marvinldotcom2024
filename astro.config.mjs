dc213ad93b4501c490cf8237bae1a15c076e3712
MarvinL.com <contact@marvinl.com>
2024-02-12 12:32:57 -0400

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), robotsTxt()],
+  integrations: [tailwind(), compress(), robotsTxt(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
