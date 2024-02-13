c9107bf6fea4f0b86dd4a1795b3a47d61cb4dfc6
MarvinL.com <contact@marvinl.com>
2024-02-13 02:45:49 -0400

    fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://www.marvinl.com",
   integrations: [tailwind(), compress(), robotsTxt(), react()],
   output: "hybrid",
   adapter: netlify()
