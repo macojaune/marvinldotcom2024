76c730be3acd5d8929005e031dae76b78302dfef
MarvinL.com <contact@marvinl.com>
2024-07-16 19:05:28 -0400

    feat: add content type to rss item

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,31 +1,33 @@
 import rss from '@astrojs/rss';
-import { getCollection } from "astro:content";
+import {getCollection} from "astro:content";
 
 export async function GET(context) {
-  const [projects, blog] = await Promise.all([getCollection("project", ({data}) => {
-    return import.meta.env.PROD ? data.isDraft !== true : true;
-  }), getCollection("blog", ({data}) => {
-    return import.meta.env.PROD ? data.isDraft !== true : true;
-  })]);
+	const [projects, blog] = await Promise.all([getCollection("project", ({data}) => {
+		return import.meta.env.PROD ? data.isDraft !== true : true;
+	}), getCollection("blog", ({data}) => {
+		return import.meta.env.PROD ? data.isDraft !== true : true;
+	})]);
 
-  return rss({
-    title: 'MarvinL.com | RSS Feed',
-    description: 'Découvre l\'actualité de MarvinL.com',
-    site: context.site,
-    items:[...projects.map((post) => ({
-          title: post.data.title,
-          pubDate: post.data.createdAt,
-          description: post.data.description,
-          // Compute RSS link from post `slug`
-          link: `/projets/${post.slug}/`,
-        })), ...blog.map((post) => ({
-              title: post.data.title,
-              pubDate: post.data.createdAt,
-              description: post.data.description,
-              // Compute RSS link from post `slug`
-              // This example assumes all posts are rendered as `/blog/[slug]` routes
-              link: `/blog/${post.slug}/`,
-            })),],
-    customData: `<language>fr-fr</language>`,
-  });
+	return rss({
+		title: 'MarvinL.com | RSS Feed',
+		description: 'Découvre l\'actualité de MarvinL.com',
+		site: context.site,
+		items: [...projects.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.createdAt,
+			description: post.data.description,
+			type: "project",
+			// Compute RSS link from post `slug`
+			link: `/projets/${post.slug}/`,
+		})), ...blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.createdAt,
+			description: post.data.description,
+			type: "blog",
+			// Compute RSS link from post `slug`
+			// This example assumes all posts are rendered as `/blog/[slug]` routes
+			link: `/blog/${post.slug}/`,
+		})),],
+		customData: `<language>fr-fr</language>`,
+	});
 }
