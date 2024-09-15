bc7dc9191353d201a1496f53b1e7c65c7ca3df13
MarvinL.com <contact@marvinl.com>
2024-09-15 05:05:10 -0400

    fix: tailwind darkMode switch

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import typography from "@tailwindcss/typography";
 export default {
-  darkMode: "selector",
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
