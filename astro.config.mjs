fecad1c51d0d171f8d7d8f05b4d9730172d8608c
MarvinL.com <contact@marvinl.com>
2024-10-17 17:05:18 -0400

    feat: add sitemap, add mdx, add static images to articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import netlify from "@astrojs/netlify";
-
 import react from "@astrojs/react";
+import mdx from '@astrojs/mdx';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site:"https://www.marvinl.com",
-  integrations: [tailwind(), compress(), robotsTxt(), react()],
+  integrations: [tailwind(), compress(), robotsTxt(), react(), mdx(), sitemap()],
   output: "hybrid",
   adapter: netlify()
 });
