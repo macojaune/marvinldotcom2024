c97bc152d81bc0638ca9071889d052bb33a3cadb
MarvinL.com <contact@marvinl.com>
2025-01-17 16:26:03 -0400

    feat: ideas

--- src/components/PitchPlayer.tsx
+++ src/components/PitchPlayer.tsx
@@ -1,84 +1,83 @@
-import { type FC, useEffect, useRef, useState } from "react";
-import { twMerge } from "tailwind-merge";
-import type { Project } from "../db/schema";
+import { type FC, useEffect, useRef, useState } from "react"
+import { twMerge } from "tailwind-merge"
+import type { Project } from "../db/schema"
 
 const PitchPlayer: FC<{
-  project: Project;
-  isResults?: boolean;
-  isPlaying: boolean;
-  onPlayToggle: () => void;
-  className?: string;
-  updateAudioProgress: (progress: number) => void;
+  project: Project
+  isResults?: boolean
+  isPlaying: boolean
+  onPlayToggle: () => void
+  className?: string
+  updateAudioProgress: (progress: number) => void
 }> = ({
   project,
   isResults = false,
   isPlaying,
   onPlayToggle,
   updateAudioProgress,
-  className = "",
+  className = ""
 }) => {
-  const [subtitles, setSubtitles] = useState(["", ""]);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [subtitles, setSubtitles] = useState(["", ""])
+  const [activeIndex, setActiveIndex] = useState(0)
 
-  const audioRef = useRef<HTMLAudioElement>(null);
+  const audioRef = useRef<HTMLAudioElement>(null)
   // Assume subtitles are in the format: [{ start: number, end: number, text: string }]
-  const subtitlesData = project?.transcript?.segments || [];
+  const subtitlesData = project?.transcript?.segments || []
 
-  const size = isResults ? 30 : 50;
+  const size = isResults ? 30 : 50
 
   useEffect(() => {
-    const audio = audioRef.current;
-    if (!audio) return;
+    const audio = audioRef.current
+    if (!audio) return
 
     const updateSubtitle = () => {
-      const currentTime = audio.currentTime;
+      const currentTime = audio.currentTime
       //update audio progress
-      const progress = currentTime / audio.duration;
-      updateAudioProgress(progress);
+      const progress = currentTime / audio.duration
+      updateAudioProgress(progress)
 
       const subtitle = subtitlesData.find(
-        (sub) => currentTime >= sub.start && currentTime <= sub.end,
-      );
-      const newSubtitle = subtitle ? subtitle.text : "";
+        (sub) => currentTime >= sub.start && currentTime <= sub.end
+      )
+      const newSubtitle = subtitle ? subtitle.text : ""
       if (newSubtitle !== subtitles[activeIndex]) {
         setSubtitles((prev) => {
-          const newSubtitles = [...prev];
-          newSubtitles[1 - activeIndex] = newSubtitle;
-          return newSubtitles;
-        });
-        setActiveIndex(1 - activeIndex);
+          const newSubtitles = [...prev]
+          newSubtitles[1 - activeIndex] = newSubtitle
+          return newSubtitles
+        })
+        setActiveIndex(1 - activeIndex)
       }
-    };
+    }
 
-    audio.addEventListener("timeupdate", updateSubtitle);
-    return () => audio.removeEventListener("timeupdate", updateSubtitle);
-  }, [subtitlesData, activeIndex, subtitles]);
+    audio.addEventListener("timeupdate", updateSubtitle)
+    return () => audio.removeEventListener("timeupdate", updateSubtitle)
+  }, [subtitlesData, activeIndex, subtitles])
 
   useEffect(() => {
     if (isPlaying) {
-      audioRef.current?.play();
+      audioRef.current?.play()
     } else {
-      audioRef.current?.pause();
+      audioRef.current?.pause()
       // audioRef.current.currentTime = 0;
     }
-  }, [isPlaying]);
+  }, [isPlaying])
+
   return (
     <div
       className={twMerge(
         "group flex items-center gap-1",
-        isResults
-          ? " h-18 w-full flex-row sm:h-24"
-          : "flex-col" + " w-full justify-between",
-        className,
+        isResults ? "w-full flex-row" : "w-full flex-col justify-between",
+        className
       )}
     >
       {isResults && (
-        <div className="relative flex h-full w-full flex-auto items-center text-center text-xs sm:text-sm">
+        <div className='relative my-4 flex h-full w-full flex-auto items-center py-4 text-center text-xs sm:text-sm'>
           {isPlaying &&
             subtitles.map((subtitle, index) => (
               <div
                 key={index}
-                className={`absolute w-full text-l/primary transition-all duration-300 ease-in-out dark:text-d/primary  ${
+                className={`absolute w-full overflow-auto text-l/primary transition-all duration-300 ease-in-out  dark:text-d/primary ${
                   index === activeIndex
                     ? "translate-y-0 transform opacity-100"
                     : "-translate-y-2 transform opacity-0"
@@ -91,59 +90,59 @@ const PitchPlayer: FC<{
       )}
       <button
         onClick={onPlayToggle}
-        className="flex aspect-square flex-1 flex-col items-center justify-center rounded-full text-center text-l/primary transition-all group-hover:scale-105 group-hover:text-l/tertiary dark:text-d/primary  group-hover:dark:text-d/tertiary"
+        className='flex aspect-square flex-1 flex-col items-center justify-center rounded-full text-center text-l/primary transition-all group-hover:scale-105 group-hover:text-l/tertiary dark:text-d/primary  group-hover:dark:text-d/tertiary'
       >
         {isPlaying ? (
           <svg
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns='http://www.w3.org/2000/svg'
             width={size}
             height={size}
-            viewBox="0 0 24 24"
-            className="group-hover:scale-110"
+            viewBox='0 0 24 24'
+            className='group-hover:scale-110'
           >
             <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 6h4v12H6V6Zm8 0h4v12h-4V6Z"
+              fill='none'
+              stroke='currentColor'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M6 6h4v12H6V6Zm8 0h4v12h-4V6Z'
             />
           </svg>
         ) : (
           <svg
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns='http://www.w3.org/2000/svg'
             width={size}
             height={size}
-            viewBox="0 0 24 24"
-            className="group-hover:scale-110"
+            viewBox='0 0 24 24'
+            className='group-hover:scale-110'
           >
             <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M7 17.259V6.741a1 1 0 0 1 1.504-.864l9.015 5.26a1 1 0 0 1 0 1.727l-9.015 5.259A1 1 0 0 1 7 17.259Z"
+              fill='none'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M7 17.259V6.741a1 1 0 0 1 1.504-.864l9.015 5.26a1 1 0 0 1 0 1.727l-9.015 5.259A1 1 0 0 1 7 17.259Z'
             />
           </svg>
         )}
         <span
           className={twMerge(
-            "text-xs text-l/tertiary dark:text-d/tertiary sm:text-sm" +
+            "text-xl text-l/tertiary dark:text-d/tertiary md:text-base" +
               " group-hover:text-l/primary" +
               " group-hover:dark:text-d/primary",
-            isResults && "text-nowrap",
+            isResults && "text-nowrap"
           )}
         >
           {isPlaying ? "Pause" : "Écouter le pitch"}
         </span>
       </button>
       {!isResults && isPlaying && (
-        <div className="relative w-full text-center text-sm min-h-[4rem] my-2">
+        <div className='relative my-2 min-h-[4rem] w-full text-center text-sm'>
           {subtitles.map((subtitle, index) => (
             <div
               key={index}
-              className={`absolute w-full text-l/primary transition-all duration-300 ease-in-out dark:text-d/primary overflow-auto ${
+              className={`absolute w-full overflow-auto text-l/primary transition-all duration-300 ease-in-out dark:text-d/primary ${
                 index === activeIndex
                   ? "translate-y-0 transform opacity-100"
                   : "-translate-y-2 transform opacity-0"
@@ -158,7 +157,7 @@ const PitchPlayer: FC<{
         <audio ref={audioRef} src={project.audioUrl} onEnded={onPlayToggle} />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default PitchPlayer;
+export default PitchPlayer
--- src/components/ProjectComparison.tsx
+++ src/components/ProjectComparison.tsx
@@ -1,51 +1,53 @@
-import React, { useEffect, useState } from "react";
-import type { Project } from "../db/schema";
-import ProjectCard from "./ProjectCard";
-import ky from "ky";
+import React, { useEffect, useState } from "react"
+import type { Project } from "../db/schema"
+import ProjectCard from "./ProjectCard"
+import ky from "ky"
 import {
   QueryClient,
   QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import PitchPlayer from "./PitchPlayer";
+  useQuery
+} from "@tanstack/react-query"
+import PitchPlayer from "./PitchPlayer"
 
 export const VoteResults = ({
   isPlaying,
   onPlayToggle,
+  updateAudioProgress
 }: {
-  isPlaying: number | null;
-  onPlayToggle: (index: number) => void;
+  isPlaying: number | null
+  onPlayToggle: (index: number) => void
+  updateAudioProgress: (progress: number) => void
 }) => {
   const { data } = useQuery<Project[]>({
     queryKey: ["projects"],
-    queryFn: async () => await ky.post("/api/projects").json(),
-  });
+    queryFn: async () => await ky.post("/api/projects").json()
+  })
 
   return (
     <div>
-      <h2 className="mb-4 text-2xl font-bold dark:text-d/primary">
+      <h2 className='mb-2 mt-8 w-fit px-2 text-2xl font-bold text-l/primary dark:bg-d/secondary dark:text-d/primary lg:mt-12'>
         Classement
       </h2>
       {data
         ?.sort((a, b) => b.votes.length - a.votes.length)
         ?.map((project, index) => {
-          const rootIndex = data.findIndex((p) => p.id === project.id);
+          const rootIndex = data.findIndex((p) => p.id === project.id)
           return (
             <div
               key={`project-result-${index}`}
-              className="mb-2 flex flex-row flex-wrap items-center gap-4 border-b border-l/primary py-2 dark:border-d/primary"
+              className='mb-2 flex flex-row flex-wrap items-center gap-4 border-b border-l/primary py-2 dark:border-d/primary'
             >
-              <div className="text-2xl font-bold dark:text-d/primary">
+              <div className='text-2xl font-bold dark:text-d/primary'>
                 #{index + 1}
               </div>
-              <div className="flex flex-col">
-                <h3 className="text-nowrap text-xl font-bold dark:text-d/primary">
+              <div className='flex flex-col'>
+                <h3 className='text-nowrap text-xl font-bold dark:text-d/primary'>
                   {project.title}
-                  <span className="ml-2 px-1 font-semibold dark:bg-d/tertiary dark:text-d/secondary">
+                  <span className='ml-2 px-1 font-semibold dark:bg-d/tertiary dark:text-d/secondary'>
                     {project.votes.length} votes
                   </span>
                 </h3>
-                <p className="text-base dark:text-d/primary">
+                <p className='text-base dark:text-d/primary'>
                   {project.description}
                 </p>
               </div>
@@ -53,79 +55,80 @@ export const VoteResults = ({
                 project={project}
                 isPlaying={isPlaying === rootIndex}
                 onPlayToggle={() => onPlayToggle(rootIndex)}
+                updateAudioProgress={updateAudioProgress}
                 isResults
               />
             </div>
-          );
+          )
         })}
     </div>
-  );
-};
+  )
+}
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export const ProjectBracket: React.FC<{
-  projects: Project[];
-  activeProjectId: number;
+  projects: Project[]
+  activeProjectId: number
 }> = ({ projects: initialProjects, activeProjectId }) => {
-  const [projects, setProjects] = useState(initialProjects);
-  const [currentPair, setCurrentPair] = useState([0, 1]);
-  const [votingHistory, setVotingHistory] = useState([]);
-  const [votingComplete, setVotingComplete] = useState(false);
-  const [comparedPairs, setComparedPairs] = useState(new Set());
-  const [showResults, toggleResults] = useState(false);
-  const [playingProject, setPlayingProject] = useState<number | null>(null);
-  const [audioProgress, setAudioProgress] = useState(0);
+  const [projects, setProjects] = useState(initialProjects)
+  const [currentPair, setCurrentPair] = useState([0, 1])
+  const [votingHistory, setVotingHistory] = useState([])
+  const [votingComplete, setVotingComplete] = useState(false)
+  const [comparedPairs, setComparedPairs] = useState(new Set())
+  const [showResults, toggleResults] = useState(false)
+  const [playingProject, setPlayingProject] = useState<number | null>(null)
+  const [audioProgress, setAudioProgress] = useState(0)
 
   useEffect(() => {
     if (
       comparedPairs.size === (projects.length * (projects.length - 1)) / 2 ||
       localStorage.getItem("voteDone") === activeProjectId.toString()
     ) {
-      setVotingComplete(true);
-      localStorage.setItem("voteDone", activeProjectId.toString());
-      toggleResults(true);
+      setVotingComplete(true)
+      localStorage.setItem("voteDone", activeProjectId.toString())
+      toggleResults(true)
     }
-  }, [comparedPairs, projects?.length]);
+  }, [comparedPairs, projects?.length])
 
   const vote = async (index: number) => {
     try {
-      await ky.post(`/api/votes/${projects[index].id}`);
-      setVotingHistory([...votingHistory, index]);
-      moveToNextPair(index);
+      await ky.post(`/api/votes/${projects[index].id}`)
+      setVotingHistory([...votingHistory, index])
+      moveToNextPair(index)
     } catch (e) {
-      console.error(e.message);
+      console.error(e.message)
     }
-  };
+  }
 
   const moveToNextPair = (currentIndex: number) => {
-    let nextIndex = (currentIndex + 1) % projects.length;
+    let nextIndex = (currentIndex + 1) % projects.length
 
     while (nextIndex !== currentIndex) {
-      const pairKey = [currentIndex, nextIndex].sort().join("-");
+      const pairKey = [currentIndex, nextIndex].sort().join("-")
       if (!comparedPairs.has(pairKey)) {
-        setCurrentPair([currentIndex, nextIndex]);
-        setComparedPairs(new Set(comparedPairs).add(pairKey));
-        return;
+        setCurrentPair([currentIndex, nextIndex])
+        setComparedPairs(new Set(comparedPairs).add(pairKey))
+        return
       }
-      nextIndex = (nextIndex + 1) % projects.length;
+      nextIndex = (nextIndex + 1) % projects.length
     }
 
     // If we've reached here, all pairs with the selected project have been compared
-    setVotingComplete(true);
-    localStorage.setItem("voteDone", activeProjectId.toString());
-    toggleResults(true);
-  };
+    setVotingComplete(true)
+    localStorage.setItem("voteDone", activeProjectId.toString())
+    toggleResults(true)
+  }
   const handlePlayToggle = (projectIndex: number) => {
-    setPlayingProject((prev) => (prev === projectIndex ? null : projectIndex));
-    setAudioProgress(0); // Reset progress when toggling
-  };
+    setPlayingProject((prev) => (prev === projectIndex ? null : projectIndex))
+    setAudioProgress(0) // Reset progress when toggling
+  }
 
   const updateAudioProgress = (progress: number) => {
-    setAudioProgress(progress);
-  };
-  const project1 = projects[currentPair[0]];
-  const project2 = projects[currentPair[1]];
+    setAudioProgress(progress)
+  }
+  const project1 = projects[currentPair[0]]
+  const project2 = projects[currentPair[1]]
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -134,17 +137,18 @@ export const ProjectBracket: React.FC<{
           <VoteResults
             isPlaying={playingProject}
             onPlayToggle={handlePlayToggle}
+            updateAudioProgress={updateAudioProgress}
           />
         ) : (
-          <div className="mx-auto my-4">
-            <h2 className="my-2 w-fit px-2 text-2xl font-bold text-l/primary dark:bg-d/secondary dark:text-d/primary">
+          <div className='mx-auto my-4'>
+            <h2 className='mb-2 mt-8 w-fit px-2 text-2xl font-bold text-l/primary dark:bg-d/secondary dark:text-d/primary lg:mt-12'>
               Vote pour le prochain projet
             </h2>
-            <p className="mb-8 text-l/primary dark:text-d/primary">
+            <p className='mb-8 text-l/primary dark:text-d/primary'>
               Une fois celui en cours mis en route, on enchaine sur celui que
               vous aurez sélectionné:
             </p>
-            <div className="mb-8 flex flex-col items-stretch justify-between sm:flex-row sm:space-x-4">
+            <div className='mb-8 flex flex-col items-stretch justify-between sm:flex-row sm:space-x-4'>
               <ProjectCard
                 project={project1}
                 onVote={() => vote(currentPair[0])}
@@ -155,7 +159,7 @@ export const ProjectBracket: React.FC<{
                 }
                 updateAudioProgress={updateAudioProgress}
               />
-              <div className="my-4 self-center text-xl font-bold text-l/primary dark:text-d/primary sm:my-0">
+              <div className='my-4 self-center text-xl font-bold text-l/primary dark:text-d/primary sm:my-0'>
                 vs.
               </div>
               <ProjectCard
@@ -169,21 +173,23 @@ export const ProjectBracket: React.FC<{
                 updateAudioProgress={updateAudioProgress}
               />
             </div>
-            <p className="text-right text-xs text-l/primary dark:text-d/primary">
+            <p className='text-right text-sm text-l/primary dark:text-d/primary'>
               Vote {comparedPairs.size + 1} sur{" "}
               {(projects.length * (projects.length - 1)) / 2}
             </p>
           </div>
         )}
-        {!votingComplete &&(<div className="flex w-full justify-center">
-          <button
-            onClick={() => toggleResults((prev) => !prev)}
-            className=" text-xs text-l/tertiary underline underline-offset-8 hover:bg-l/tertiary hover:text-l/bg hover:no-underline focus:text-l/primary focus-visible:text-l/primary active:text-l/primary dark:text-d/primary dark:hover:bg-d/tertiary dark:hover:text-d/secondary dark:focus:text-d/primary dark:focus-visible:text-d/primary dark:active:text-d/primary"
-          >
-            {showResults ? "Retour au vote" : "Flemme… montre les résultats"}
-          </button>
-        </div>)}
+        {!votingComplete && (
+          <div className='flex w-full justify-center'>
+            <button
+              onClick={() => toggleResults((prev) => !prev)}
+              className='text-base text-l/tertiary underline underline-offset-8 hover:bg-l/tertiary hover:text-l/bg hover:no-underline focus:text-l/primary focus-visible:text-l/primary active:text-l/primary dark:text-d/primary dark:hover:bg-d/tertiary dark:hover:text-d/secondary dark:focus:text-d/primary dark:focus-visible:text-d/primary dark:active:text-d/primary'
+            >
+              {showResults ? "Retour au vote" : "Flemme… montre les résultats"}
+            </button>
+          </div>
+        )}
       </>
     </QueryClientProvider>
-  );
-};
+  )
+}
